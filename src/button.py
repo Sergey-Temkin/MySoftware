e121c31e5a8f5c65d7701d5ff73d4374f938027d
Sergey-Temkin <sergheytemkin@gmail.com>
2025-08-27 20:20:30 +0300

    chore: update click() message

--- src/button.py
+++ src/button.py
@@ -1,9 +1,9 @@
 # src/button.py
 # Simple function that prints "Click" to stdout.
 def click():
-    print("Click")
+    print("Click button updated!")
+
 
 # Quick manual test when running this file directly:
 if __name__ == "__main__":
     click()
-
